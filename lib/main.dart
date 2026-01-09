5345ecee6e3058fbdf180aac07c711ce427512e9
Kaleb Newton <kalebnewton@Kalebs-MacBook-Pro.local>
2026-01-09 16:37:33 -0500

    feat: converted StatefulWidget and implemented cart logic

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyProfileApp());
 
-class MyProfileApp extends StatelessWidget {
+class MyProfileApp extends StatefulWidget {
   const MyProfileApp({super.key});
 
   @override
+  State<MyProfileApp> createState() => _MyProfileAppState();
+}
+
+class _MyProfileAppState extends State<MyProfileApp> {
+  @override
+  int cartCount = 0;
+  double totalPrice = 0.0;
+  int removecartCount = -1;
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -15,6 +24,10 @@ class MyProfileApp extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Text(
+                  'Cart: $cartCount items | Total: \$${totalPrice.toStringAsFixed(2)}',
+                  style: const TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                ),
                 // 1. The Avatar (A Container box)
                 const CircleAvatar(
                   radius: 50.0,
@@ -39,7 +52,11 @@ class MyProfileApp extends StatelessWidget {
                   children: [
                     ElevatedButton.icon(
                       onPressed: () {
-                      print("Added to Cart!");
+                      setState(() {
+                        cartCount++; // Increment the count
+                        totalPrice += 299.99; // Add the price of the headphones
+                      });
+                      print("Current Total: \$$totalPrice");
                     }, 
                     icon: const Icon(Icons.shopping_cart, size: 18),
                     label: const Text('Add to Cart'),
@@ -49,11 +66,39 @@ class MyProfileApp extends StatelessWidget {
                       onPressed: (){
                       print('Details Clicked');
                     }, 
-                    
                     child: const Text('Details')
                     )
                   ],
-                )
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        // Subtract if there are items in the cart
+                      setState(() {
+                        if (cartCount >0 ){
+                          cartCount--; // Increment the count
+                          totalPrice -= 299.99; // Add the price of the headphones
+                        }
+                      });
+                      print("Current Total: \$$totalPrice");
+                    }, 
+                    icon: const Icon(Icons.shopping_cart, size: 18),
+                    label: const Text('Remove from Cart'),
+                    ),
+                    const SizedBox(width: 10), //Horizontal gap between buttons
+                    OutlinedButton(
+                      onPressed: (){
+                      setState(() {
+                        cartCount = 0;
+                        totalPrice = 0;
+                      });
+                    }, 
+                    child: const Text('Empty Cart')
+                    )
+                  ],
+                ),
               ], //children
             ),
           ),
