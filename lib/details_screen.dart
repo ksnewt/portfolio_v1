04d9cab97f098e07aed40bd16c8f2b5e0a995eee
Kaleb Newton <kalebnewton@Mac.fios-router.home>
2026-01-22 18:31:28 -0500

    WIP: Starting data pass to DetailsScreen (Logic half-finished)

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({super.key});
+  // Defining variables that the screen needs to recieve
+  final String productName;
+  final double productPrice;
+
+  // Add them to the "Constuctor" (Envelope)
+  const DetailsScreen({
+    super.key,
+    required this.productName,
+    required this.productPrice,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Product Details'),
-        backgroundColor: Colors.blueGrey[900],
-      ),
-      backgroundColor: Colors.blueGrey[800],
+      appBar: AppBar(title: const Text((productName)),
       body: const Center(
-        child: Text(
+        child: Column()
           'Detailed View of Headphones\n\nBluetooth 6.0 | 40hr Battery',
           textAlign: TextAlign.center,
           style: TextStyle(color: Colors.white, fontSize: 18),
